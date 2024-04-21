56ec2bf410bb121533a6954dbba2fc8512c35d06
gvs89 <112775497+gvs89@users.noreply.github.com>
2024-04-21 22:32:09 +0300

    add

--- dz10/dz10.py
+++ dz10/dz10.py
@@ -4,9 +4,11 @@ import random
 lst = ['robot'] * 10
 lst += ['human'] * 10
 random.shuffle(lst)
-
 data = pd.DataFrame({'whoAmI': lst})
 
-one_hot_encoded = pd.get_dummies(data, columns=['whoAmI'])
-
-print(one_hot_encoded.head())
+categories = data['whoAmI'].unique()
+one_hot = pd.DataFrame(columns=categories)
+for index, row in data.iterrows():
+    category = row['whoAmI']
+    one_hot.loc[index, category] = 1
+print(one_hot)
